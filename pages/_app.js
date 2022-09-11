31960a34ec39b72821111869e14a1706a5693fa6
AOMuiz <abdulwasiumuiz@gmail.com>
2022-09-11 11:08:26 +0100

    fix: remove viewport-fit=cover from meta viewport crashing mobile layout

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta charset="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>
         {/* <ReadingBar /> */}
